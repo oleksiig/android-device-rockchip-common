a78e901fd55082b1b64a0a13aa89290435f29973
Oleksii Gulchenko <alexey.gulchenko@gmail.com>
2022-10-02 10:08:37 +0200

    device-common.mk: Do not use default "rockchip" HALs for all devices
    Signed-off-by: Oleksii Gulchenko <alexey.gulchenko@gmail.com>

--- device-common.mk
+++ device-common.mk
@@ -61,13 +61,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-impl \
-    android.hardware.keymaster@3.0-service \
-    android.hardware.gatekeeper@1.0-service.rockchip
-
-PRODUCT_PACKAGES += \
-    android.hardware.power@1.0-service.rockchip \
-    android.hardware.health@2.0-service.rockchip \
-    android.hardware.memtrack@1.0-service.rockchip
+    android.hardware.keymaster@3.0-service
 
 # Copy software config file(s)
 PRODUCT_COPY_FILES += \
