4060f28ce7672615bcdd50f5cc63e7a5d495b814
Oleksii Gulchenko <alexey.gulchenko@gmail.com>
2023-12-05 18:44:33 +0100

    Integrated IRQBalance tool from github.com/ix5/android-irqbalance
    https://github.com/ix5/android-irqbalance/tree/master
    
    Signed-off-by: Oleksii Gulchenko <alexey.gulchenko@gmail.com>

--- device-common.mk
+++ device-common.mk
@@ -133,3 +133,8 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.vulkan.version-1_1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.version-1_1.xml \
     frameworks/native/data/etc/android.hardware.vulkan.level-0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.level-0.xml \
     frameworks/native/data/etc/android.hardware.vulkan.compute-0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.compute-0.xml
+
+# irqbalance daemon
+PRODUCT_PACKAGES += irqbalance
+PRODUCT_COPY_FILES += \
+    device/rockchip/$(TARGET_PRODUCT)/irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/irqbalance.conf
